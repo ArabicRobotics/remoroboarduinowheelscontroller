eee9c539e7b9314e4b920abfbb82983c16bc02d5
TareqDesktop <ArabicRobotics@gmail.com>
2022-07-06 15:27:58 +0800

    modify the server

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-﻿import imp
+﻿from concurrent.futures import thread
+import imp
+import threading
 from clsSocketServer import SocketSever
 import sys
 from clsInputCatcher import InputCatcher
 from clsRobot import Robot
 def main():
 	server = SocketSever()
-	server.Start()
+	t= threading.Thread(server.Start)
+	t.start()
 
 # Message Example     {"com": "m","params": [1,-200,3,4], "requestId": "2"}  
 if __name__ == "__main__":
